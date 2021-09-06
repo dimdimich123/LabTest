20ee30a319632ad02afffaea0859c13790ad5de8
dimdimich123 <frostm333@users.noreply.github.com>
2021-09-06 21:01:31 +0700

    Add JavaDoc

--- app/src/main/java/ru/chepurkoempty/lab1tests/MainActivity.java
+++ app/src/main/java/ru/chepurkoempty/lab1tests/MainActivity.java
@@ -1,15 +1,24 @@
 package ru.chepurkoempty.lab1tests;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    public int max(int x, int y){
+    /**
+     * max (Максимум) - возвращает максимальное значение из переданных параметров
+     * @param x - целочисленоя переменная
+     * @param y - целочисленоя переменная
+     */
+    public int max(int x, int y) {
         return x < y ? y : x;
     }
 
+    /**
+     * min (Минимум) - возвращает минимальное значение из переданных параметров
+     * @param x - целочисленоя переменная
+     * @param y - целочисленоя переменная
+     */
     public int min(int x, int y){
         return x < y ? x : y;
     }
